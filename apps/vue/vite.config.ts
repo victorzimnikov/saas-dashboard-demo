83b0366d19b098294adb4c7042887766229570df
Victor Zimnikov <victor.zimnikov@yarndex.ru>
2026-08-31 12:13:22 +0200

    chore: changed vite config

--- apps/vue/vite.config.ts
+++ apps/vue/vite.config.ts
@@ -1,6 +1,7 @@
 import Vue from "@vitejs/plugin-vue";
 import VueRouter from "vue-router/vite";
 import { defineConfig } from "vite";
+// import { readFileSync } from "node:fs";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -9,6 +10,13 @@ export default defineConfig({
   },
   server: {
     host: "0.0.0.0",
+    port: 5173,
+    // strictPort: true,
+
+    // https: {
+    //   key: readFileSync(new URL("./.cert/dev-key.pem", import.meta.url)),
+    //   cert: readFileSync(new URL("./.cert/dev-cert.pem", import.meta.url)),
+    // },
   },
   plugins: [VueRouter(), Vue()],
 });
